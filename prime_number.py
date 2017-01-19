8c5e26f6ebbf6a55ee36440022e3a591f9b1c7d8
Lewis Sawe <lewisbet9@gmail.con>
2017-01-20 00:07:05 +0300

    Refined the generation of prime numbers

--- prime_number.py
+++ prime_number.py
@@ -1,9 +1,12 @@
-def prime_number(x):
-    if x >= 2:
-        for y in range(2,x):
-            if not ( x % y ):
-                return False
-    else:
-        return False
-    return True
-	        
+def generate_primes(n):
+
+	primes = []
+	for num in range (2, n + 1):
+		isPrime = True
+		for x in range (2, num):
+			if num % x == 0:
+				isPrime = False
+		if isPrime:
+			primes.append(num)
+
+	return primes
