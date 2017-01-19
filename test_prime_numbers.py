05aadb58dd5201d071c9caf2dffaa24055236465
Lewis Sawe <lewisbet9@gmail.con>
2017-01-19 23:56:27 +0300

    Add extra tests

--- test_prime_numbers.py
+++ test_prime_numbers.py
@@ -3,14 +3,20 @@ from primeNumber import *
 class primeNumber(unittest.TestCase):
     def test_is_prime(self):
         self.assertTrue(is_prime(5))
-        self.assertTrue(is_prime(8))
-        self.assertFalse(is_prime(7))
-        self.assertTrue(is_prime(3))
-        self.assertFalse(is_prime(67))
-        self.assertFalse(is_prime(99))
-        self.assertTrue(is_prime(100))
-        self.assertTrue(is_prime(54))
-        self.assertTrue(is_prime(1))
-        self.assertTrue(is_prime(47))
+
+    def test_no_input(self):
+        self.assertIsNone(is_prime(None))
+
+    def test_is_integer(self):
+        self.assertIsInstance(is_prime(5, int))
+
+    def test_primein_0_to_negativeInt(self):
+        with self.assertRaises(TypeError):
+            is_prime(-20)
+
+    def test_NonInteger_input(self):
+        with self.assertRaises(TypeError):
+            is_prime([])
+
 if __name__ == '__main__':
     
